f06a3a087bc48debac15e8c7ed7583945a3d18b0
Barak Saidoff <barak.saidoff@gmail.com>
2020-08-10 12:42:58 -0400

    move state to app component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,90 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import CalculatorCard from './containers/Card'
 import SNCOLogo from './components/SNCOLogo'
 
-function App() {
-  return (
-    <div className="App">
-      <SNCOLogo/>
-      <CalculatorCard></CalculatorCard>
-    </div>
-  );
+class App extends React.Component {
+
+  state={
+    dropDownChoice: "Multifamily Max Refi",
+    address: "",
+    purchasePrice: "",
+    requestLoanAmount: "",
+    date: new Date(),
+    annualGrossRent: "",
+    units: "",
+    vacancy: "",
+    taxes: "",
+    insurance: "",
+    waterSewer: "",
+    utilities: "",
+    management: "",
+    replacementReserves: "",
+    hardCosts: "",
+    softCosts: "",
+    // totalProjectCost: 0,
+
+}
+
+handleAddressChange=(e)=>{
+  this.setState({
+      address: e.target.value
+  },()=>console.log(this.state.address))
+}
+
+
+handleNumberChange=(e)=>{
+
+  // let typedValue = e.target.value
+  // if (typedValue === ""){
+  //     typedValue = 0;
+  // } 
+  this.setState({
+      [e.target.name]: parseInt(e.target.value)
+  })
+}
+
+
+changeDropDownChoice=(e)=>{
+  this.setState({
+      dropDownChoice: e
+  })
+}
+
+dateChange = date => this.setState({ date })
+
+
+
+  render(){
+    return (
+      <div className="App">
+        <SNCOLogo/>
+        <CalculatorCard
+          dropDownChoice={this.state.dropDownChoice}
+          address= {this.state.address}
+          purchasePrice= {this.state.purchasePrice}
+          requestLoanAmount= {this.state.requestLoanAmount}
+          date= {this.state.date}
+          annualGrossRent= {this.state.annualGrossRent}
+          units= {this.state.units}
+          vacancy= {this.state.vacancy}
+          taxes= {this.state.taxes}
+          insurance= {this.state.insurance}
+          waterSewer= {this.state.waterSewer}
+          utilities= {this.state.utilities}
+          management= {this.state.management}
+          replacementReserves= {this.state.replacementReserves}
+          hardCosts= {this.state.hardCosts}
+          softCosts= {this.state.softCosts}
+
+          dateChange={this.dateChange}
+          changeDropDownChoice={this.changeDropDownChoice}
+          handleNumberChange={this.handleNumberChange}
+          handleAddressChange={this.handleAddressChange}
+        />
+      </div>
+    );
+
+
+  }
 }
 
 export default App;
--- src/containers/Card.js
+++ src/containers/Card.js
@@ -7,53 +7,53 @@ import GeoCode from '../components/GeoCode'
 
 export default class CalculatorCard extends Component {
 
-    state={
-        dropDownChoice: "Multifamily Max Refi",
-        address: "",
-        purchasePrice: "",
-        requestLoanAmount: "",
-        date: new Date(),
-        monthlyGrossRent: "",
-        units: "",
-        vacancy: "",
-        taxes: "",
-        insurance: "",
-        waterSewer: "",
-        utilities: "",
-        management: "",
-        replacementReserves: "",
-        hardCosts: "",
-        softCosts: "",
+    // state={
+    //     dropDownChoice: "Multifamily Max Refi",
+    //     address: "",
+    //     purchasePrice: "",
+    //     requestLoanAmount: "",
+    //     date: new Date(),
+    //     monthlyGrossRent: "",
+    //     units: "",
+    //     vacancy: "",
+    //     taxes: "",
+    //     insurance: "",
+    //     waterSewer: "",
+    //     utilities: "",
+    //     management: "",
+    //     replacementReserves: "",
+    //     hardCosts: "",
+    //     softCosts: "",
         // totalProjectCost: 0,
 
-    }
+    // }
 
-    handleAddressChange(e){
-        this.setState({
-            address: e.target.value
-        },()=>console.log(this.state.address))
-    }
-
-
-    handleNumberChange(e){
-
-        let typedValue = e.target.value
-        if (typedValue === ""){
-            typedValue = 0;
-        } 
-        this.setState({
-            [e.target.name]: parseInt(typedValue)
-        })
-    }
+    // handleAddressChange(e){
+    //     this.setState({
+    //         address: e.target.value
+    //     },()=>console.log(this.state.address))
+    // }
 
 
-    changeDropDownChoice(e){
-        this.setState({
-            dropDownChoice: e
-        })
-    }
+    // handleNumberChange(e){
+
+    //     // let typedValue = e.target.value
+    //     // if (typedValue === ""){
+    //     //     typedValue = 0;
+    //     // } 
+    //     this.setState({
+    //         [e.target.name]: parseInt(e.target.value.toLocaleString())
+    //     })
+    // }
+
+
+    // changeDropDownChoice(e){
+    //     this.setState({
+    //         dropDownChoice: e
+    //     })
+    // }
       
-    dateChange = date => this.setState({ date })
+    // dateChange = date => this.setState({ date })
 
 
     // calculateTotalProjectCost(){
@@ -71,10 +71,24 @@ export default class CalculatorCard extends Component {
              
     // }
 
-    dateChange = date => this.setState({ date })
+    // dateChange = date => this.setState({ date })
 
 
     render() {
+
+        let officeExpenses = (this.props.units * 500)
+        let replacementReserves = (this.props.units * 250)
+
+        let management = (this.props.annualGrossRent * .04)
+
+        let taxes = parseInt(this.props.taxes *1)
+        let utilities = parseInt(this.props.utilities *1)
+        let waterSewer = parseInt(this.props.waterSewer *1)
+
+        let totalIncomeGenerated = (((this.props.annualGrossRent + (officeExpenses) + (replacementReserves))) *1);
+        let grossAnnualOperatingExpenses = (taxes + utilities + waterSewer + management);
+        let noi = (totalIncomeGenerated - grossAnnualOperatingExpenses);
+        let capRate = ((noi / this.props.purchasePrice) * 100)
 
        
         return (
@@ -83,8 +97,8 @@ export default class CalculatorCard extends Component {
                     <Card.Text>SNCO Calculator</Card.Text>
                         <DropdownButton 
                             variant="dark"
-                            title={this.state.dropDownChoice} 
-                            onSelect= {(e)=>this.changeDropDownChoice(e)}
+                            title={this.props.dropDownChoice} 
+                            onSelect= {(e)=>this.props.changeDropDownChoice(e)}
                             style={{ margin:7.5 }}
                         >
                             <Dropdown.Item eventKey="Multifamily Max Refi">Multifamily Max Refi</Dropdown.Item>
@@ -100,38 +114,38 @@ export default class CalculatorCard extends Component {
                                         <Card.Body>
                                     
                                                 
-                                                    {/* <FormControl aria-label="Address" placeholder="Address" name="address" value={this.state.address} onChange={(e)=>this.handleAddressChange(e)} style={{margin:7.5}}></FormControl> */}
+                                                    {/* <FormControl aria-label="Address" placeholder="Address" name="address" value={this.props.address} onChange={(e)=>this.handleAddressChange(e)} style={{margin:7.5}}></FormControl> */}
 
                                                     <GeoCode></GeoCode>
                                                 
                                                 <InputGroup>
-                                                    <FormControl placeholder="Purchase Price" name="purchasePrice" value={this.state.purchasePrice} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
-                                                    <FormControl placeholder="Requested Loan Amount" name="requestLoanAmount" value={this.state.requestLoanAmount} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Purchase Price" name="purchasePrice" value={this.props.purchasePrice} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Requested Loan Amount" name="requestLoanAmount" value={this.props.requestLoanAmount} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
                                                     <DatePicker
                                                     onChange={this.dateChange}
-                                                    value={this.state.date}
+                                                    value={this.props.date}
                                                     />
                                                 </InputGroup>
                                                 <InputGroup>
-                                                    <FormControl placeholder="Monthly Gross Rent" name="monthlyGrossRent" value={this.state.monthlyGrossRent} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
-                                                    <FormControl placeholder="Units" name="units" value={this.state.units} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
-                                                    <FormControl placeholder="Vacancy" name="vacancy" disabled={true} value={(parseInt(this.state.monthlyGrossRent) * .03)} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Annual Gross Rent" name="annualGrossRent" value={this.props.annualGrossRent} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Units" name="units" value={this.props.units} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Vacancy" name="vacancy" disabled={true} value={(parseInt(this.props.annualGrossRent) * .03)} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
                                                 </InputGroup>
                                                 <InputGroup>
-                                                    <FormControl placeholder="Taxes" type="number" name="taxes" value={this.state.taxes} onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
-                                                    <FormControl placeholder="Insurance"  name="insurance" value={this.state.insurance} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Taxes" type="number" name="taxes" value={this.props.taxes} onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Insurance"  name="insurance" value={this.props.insurance} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
                                                 </InputGroup>
                                                 <InputGroup>
-                                                    <FormControl placeholder="Water Sewer" name="waterSewer" value={this.state.waterSewer} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
-                                                    <FormControl placeholder="Utilities" name="utilities" value={this.state.utilities} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Water Sewer" name="waterSewer" value={this.props.waterSewer} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Utilities" name="utilities" value={this.props.utilities} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
                                                 </InputGroup>
                                                 <InputGroup>
-                                                    <FormControl placeholder="Management" name="management" disabled={true} value={(parseInt(this.state.monthlyGrossRent) * .04)} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
-                                                    <FormControl placeholder="Replacement Reserves" name="replacementReserves" value={this.state.replacementReserves} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Management" name="management" disabled={true} value={management} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Replacement Reserves" name="replacementReserves" value={this.props.replacementReserves} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
                                                 </InputGroup>
                                                 <InputGroup>
-                                                    <FormControl placeholder="Hard Costs" name="hardCosts" value={this.state.hardCosts} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
-                                                    <FormControl placeholder="Soft Costs" name="softCosts" value={this.state.softCosts} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Hard Costs" name="hardCosts" value={this.props.hardCosts} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
+                                                    <FormControl placeholder="Soft Costs" name="softCosts" value={this.props.softCosts} type="number" onChange={(e)=>this.props.handleNumberChange(e)} style={{margin:7.5}}></FormControl>
                                                 </InputGroup>
 
                                 
@@ -143,11 +157,16 @@ export default class CalculatorCard extends Component {
                                     <Card style={{  border: '2px solid #B98757', margin: "1rem", borderRadius: 15 }}>
                                         <Card.Body>
                                             <Card.Text style={{textAlign: "left"}}>
-                                            <h5>{`Total project cost: $${(((this.state.purchasePrice) + (this.state.hardCosts) + (this.state.softCosts))* 1).toLocaleString()}`}</h5>
-                                            <h5>Taxes: ${ (((this.state.taxes)* .03)+ this.state.taxes)}</h5>
-                                            <h5>Office Expenses: ${this.state.units * 500}</h5>
-                                            <h5>Replacement Reserves: ${this.state.units * 250}</h5>
-                                            <h5>Management: ${this.state.monthlyGrossRent * .04}</h5>
+                                            <h5>{`Total project cost: $${(((this.props.purchasePrice) + (this.props.hardCosts) + (this.props.softCosts))* 1).toLocaleString()}`}</h5>
+                                            <h5>Taxes: ${ (((this.props.taxes)* .03)+ this.props.taxes)}</h5>
+                                            <h5>Office Expenses: ${officeExpenses.toLocaleString()}</h5>
+                                            <h5>Replacement Reserves: ${replacementReserves.toLocaleString()}</h5>
+                                            <h5>Management: ${management.toLocaleString()}</h5>
+                                            <h5>Insurance: ${(this.props.insurance *1)}</h5>
+                                            <h5>Gross Annual Income: ${totalIncomeGenerated.toLocaleString()}</h5>
+                                            <h5>Gross Annual Operating Expenses: ${ grossAnnualOperatingExpenses.toLocaleString()}</h5>
+                                            <h5>NOI: ${noi.toLocaleString()}</h5>
+                                            <h5>Cap Rate: { capRate ? capRate : 0  }</h5>
                                             </Card.Text>
                                         </Card.Body>
                                     </Card>
