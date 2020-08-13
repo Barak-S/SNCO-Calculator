3c76e50f42eabfffde3b564d59af14526b2b9dcd
Barak Saidoff <barak.saidoff@gmail.com>
2020-08-13 10:19:39 -0400

    disabled input for results

--- src/containers/FixAndFlip.js
+++ src/containers/FixAndFlip.js
@@ -59,129 +59,137 @@ export default class FixAndFlip extends Component {
         return (
 
 
-            <Container fluid>
-                <Row>
+                <Container fluid>
+                    <Row>
 
-                    <Col md={7}>
-                        <Card style={{ border: '2.5px solid #B98757', margin: "1rem", borderRadius: 15  }}>
-                            <Card.Body>
-                        
-                                <GeoCode></GeoCode>
-                                
-                                <InputGroup className="mb-3">
-                                    <InputGroup.Prepend>
-                                    <InputGroup.Text>Purchase Price</InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                        <FormControl name="purchasePrice" value={ this.state.purchasePrice || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{marginRight: 7}}></FormControl>
-                                    <InputGroup.Prepend>
-                                    <InputGroup.Text>Renovation Costs</InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                        <FormControl name="renovation" value={ this.state.renovation || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)}></FormControl>
-                                </InputGroup>
-                                <InputGroup className="mb-3">
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text>ARV</InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                    <FormControl name="arv" value={ this.state.arv || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)}></FormControl>
-                                </InputGroup>
-                                <InputGroup className="mb-3">
-                                {/* <InputGroup.Prepend>
-                                    <InputGroup.Text>Exit Strategy</InputGroup.Text>
-                                    </InputGroup.Prepend> */}
-                                    <DropdownButton
-                                        as={InputGroup.Append}
-                                        variant="outline-secondary"
-                                        onSelect= {(e)=>this.changeExitStrategy(e)}
-                                        title="Exit Strategy"
-                                        id="input-group-dropdown-1"
-                                        >
-                                        <Dropdown.Item eventKey="Fix & Flip">Fix & Flip</Dropdown.Item>
-                                        <Dropdown.Item eventKey="Fix & Refinance">Fix & Refinance</Dropdown.Item>
-                                    </DropdownButton>
-                                    <FormControl name="exitStrategy" value={ this.state.exitStrategy || undefined} type="text" disabled={true} onChange={(e)=>this.handleNumberChange(e)} ></FormControl>
-                                    </InputGroup>
-
-                                    {this.state.exitStrategy === "Fix & Flip" ? 
+                        <Col md={7}>
+                            <Card style={{ border: '2.5px solid #B98757', margin: "1rem", borderRadius: 15  }}>
+                                <Card.Body>
+                            
+                                    <GeoCode></GeoCode>
                                     
-                                        
-                                    <div>
-                                        <InputGroup className="mb-3">
-                                            <InputGroup.Prepend>
-                                            <InputGroup.Text>Carrying Costs</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                                <FormControl name="carryCosts" value={ this.state.carryCosts || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{marginRight: 7}}></FormControl>
-                                                <InputGroup.Prepend>
-                                            <InputGroup.Text>Closing Costs</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                                <FormControl name="closingCosts" value={ this.state.closingCosts || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)}></FormControl>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                        <InputGroup.Text>Purchase Price</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                            <FormControl name="purchasePrice" value={ this.state.purchasePrice || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{marginRight: 7}}></FormControl>
+                                        <InputGroup.Prepend>
+                                        <InputGroup.Text>Renovation Costs</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                            <FormControl name="renovation" value={ this.state.renovation || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)}></FormControl>
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text>ARV</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl name="arv" value={ this.state.arv || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)}></FormControl>
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <DropdownButton
+                                            as={InputGroup.Append}
+                                            variant="outline-secondary"
+                                            onSelect= {(e)=>this.changeExitStrategy(e)}
+                                            title="Exit Strategy"
+                                            id="input-group-dropdown-1"
+                                            >
+                                            <Dropdown.Item eventKey="Fix & Flip">Fix & Flip</Dropdown.Item>
+                                            <Dropdown.Item eventKey="Fix & Refinance">Fix & Refinance</Dropdown.Item>
+                                        </DropdownButton>
+                                        <FormControl name="exitStrategy" value={ this.state.exitStrategy || undefined} type="text" disabled={true} onChange={(e)=>this.handleNumberChange(e)} ></FormControl>
                                         </InputGroup>
-                                        <InputGroup className="mb-3">
-                                            {/* <InputGroup.Prepend>
-                                            <InputGroup.Text>Experience Level</InputGroup.Text>
-                                            </InputGroup.Prepend> */}
+
+                                        {this.state.exitStrategy === "Fix & Flip" ? 
+                                        
+                                            
+                                        <div>
+                                            <InputGroup className="mb-3">
+                                                <InputGroup.Prepend>
+                                                <InputGroup.Text>Carrying Costs</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                                    <FormControl name="carryCosts" value={ this.state.carryCosts || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)} style={{marginRight: 7}}></FormControl>
+                                                    <InputGroup.Prepend>
+                                                <InputGroup.Text>Closing Costs</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                                    <FormControl name="closingCosts" value={ this.state.closingCosts || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)}></FormControl>
+                                            </InputGroup>
+                                            <InputGroup className="mb-3">
+                                                    <DropdownButton
+                                                        as={InputGroup.Append}
+                                                        variant="outline-secondary"
+                                                        onSelect= {(e)=>this.changeExperience(e)}
+                                                        title="Experience Level"
+                                                        id="input-group-dropdown-1"
+                                                        >
+                                                        <Dropdown.Item eventKey="0 properties">0 properties</Dropdown.Item>
+                                                        <Dropdown.Item eventKey="3 properties">3 properties</Dropdown.Item>
+                                                        <Dropdown.Item eventKey="6 properties">6 properties</Dropdown.Item>
+                                                    </DropdownButton>
+
+                                                    <FormControl name="experienceLevel" value={ this.state.experienceLevel || undefined} type="text" disabled={true} onChange={(e)=>this.handleNumberChange(e)} style={{marginRight: 7}}></FormControl>
+                                                    <InputGroup.Prepend>
+                                                <InputGroup.Text>Credit Score</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                                    <FormControl name="creditScore" value={ this.state.creditScore || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)} ></FormControl>
+                                            </InputGroup>
+                                            <InputGroup className="mb-3">
                                                 <DropdownButton
                                                     as={InputGroup.Append}
                                                     variant="outline-secondary"
-                                                    onSelect= {(e)=>this.changeExperience(e)}
-                                                    title="Experience Level"
+                                                    onSelect= {(e)=>this.changeturnaroundTime(e)}
+                                                    title="Turnaround Time"
                                                     id="input-group-dropdown-1"
                                                     >
-                                                    <Dropdown.Item eventKey="0 properties">0 properties</Dropdown.Item>
-                                                    <Dropdown.Item eventKey="3 properties">3 properties</Dropdown.Item>
-                                                    <Dropdown.Item eventKey="6 properties">6 properties</Dropdown.Item>
+                                                    <Dropdown.Item eventKey="3 Months">3 Months</Dropdown.Item>
+                                                    <Dropdown.Item eventKey="6 Months">6 Months</Dropdown.Item>
+                                                    <Dropdown.Item eventKey="12 Months">12 Months</Dropdown.Item>
+                                                    <Dropdown.Item eventKey="18 Months">18 Months</Dropdown.Item>
+                                                    <Dropdown.Item eventKey="24 Months">24 Months</Dropdown.Item>
                                                 </DropdownButton>
+                                                    <FormControl name="turnaroundTime" value={ this.state.turnaroundTime || undefined} type="text" disabled={true} onChange={(e)=>this.handleNumberChange(e)} style={{marginRight: 7}}></FormControl>
+                                                    <InputGroup.Prepend>
+                                                <InputGroup.Text>Resell Costs</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                                    <FormControl name="resellCosts" value={ this.state.resellCosts || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)}></FormControl>
+                                            </InputGroup>
+                                        </div> 
+                                            : 
+                                        null}
 
-                                                <FormControl name="experienceLevel" value={ this.state.experienceLevel || undefined} type="text" disabled={true} onChange={(e)=>this.handleNumberChange(e)} style={{marginRight: 7}}></FormControl>
-                                                <InputGroup.Prepend>
-                                            <InputGroup.Text>Credit Score</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                                <FormControl name="creditScore" value={ this.state.creditScore || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)} ></FormControl>
-                                        </InputGroup>
-                                        <InputGroup className="mb-3">
-                                        {/* <InputGroup.Prepend>
-                                            <InputGroup.Text>Turnaround Time</InputGroup.Text>
-                                            </InputGroup.Prepend> */}
-                                            <DropdownButton
-                                                as={InputGroup.Append}
-                                                variant="outline-secondary"
-                                                onSelect= {(e)=>this.changeturnaroundTime(e)}
-                                                title="Turnaround Time"
-                                                id="input-group-dropdown-1"
-                                                >
-                                                <Dropdown.Item eventKey="3 Months">3 Months</Dropdown.Item>
-                                                <Dropdown.Item eventKey="6 Months">6 Months</Dropdown.Item>
-                                                <Dropdown.Item eventKey="12 Months">12 Months</Dropdown.Item>
-                                                <Dropdown.Item eventKey="18 Months">18 Months</Dropdown.Item>
-                                                <Dropdown.Item eventKey="24 Months">24 Months</Dropdown.Item>
-                                            </DropdownButton>
-                                                <FormControl name="turnaroundTime" value={ this.state.turnaroundTime || undefined} type="text" disabled={true} onChange={(e)=>this.handleNumberChange(e)} style={{marginRight: 7}}></FormControl>
-                                                <InputGroup.Prepend>
-                                            <InputGroup.Text>Resell Costs</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                                <FormControl name="resellCosts" value={ this.state.resellCosts || undefined} type="number" onChange={(e)=>this.handleNumberChange(e)}></FormControl>
-                                        </InputGroup>
-                                    </div> 
-                                        : 
-                                    null}
+                                </Card.Body>
+                            </Card>
+                        </Col>
 
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col md={5}>
-                        <Card style={{  border: '2.5px solid #B98757', margin: "1rem", borderRadius: 15 }}>
-                            <Card.Body style={{textAlign: "left", fontWeight: "600"}}>
-                                
-                                <Card.Text>Total In: { this.numberFormat(totalIn) }</Card.Text>
-                                <Card.Text>Total Profit on Flip: { this.numberFormat(totalProfit) }</Card.Text>
-                                <Card.Text>Profit Percent: %{ profitPercent ? profitPercent.toFixed(2) : 0 }</Card.Text>
+                        <Col md={5}>
+                            <Card style={{  border: '2.5px solid #B98757', margin: "1rem", borderRadius: 15 }}>
+                                <Card.Body style={{textAlign: "left", fontWeight: "600"}}>
                                     
-                            </Card.Body>
-                        </Card>
-                    
-                    </Col>
-                </Row>
-            </Container>
+                                    {/* <Card.Text>Total In: { this.numberFormat(totalIn) }</Card.Text>
+                                    <Card.Text>Total Profit on Flip: { this.numberFormat(totalProfit) }</Card.Text>
+                                    <Card.Text>Profit Percent: %{ profitPercent ? profitPercent.toFixed(2) : 0 }</Card.Text> */}
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                        <InputGroup.Text style={{fontWeight: "600"}}>Total In:</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                            <FormControl value={ totalIn? (totalIn) : 0 || undefined} type="number" disabled={true} onChange={(e)=>this.props.handleNumberChange(e)}></FormControl>
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                        <InputGroup.Text style={{fontWeight: "600"}}>Total Profit on Flip:</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                            <FormControl value={ totalProfit? (totalProfit) : 0 || undefined} type="number" disabled={true} onChange={(e)=>this.props.handleNumberChange(e)}></FormControl>
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                        <InputGroup.Text style={{fontWeight: "600"}}>Profit Percent: %</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                            <FormControl value={ profitPercent? profitPercent.toFixed(2) : 0 || undefined} type="number" disabled={true} onChange={(e)=>this.props.handleNumberChange(e)}></FormControl>
+                                    </InputGroup>
+                                </Card.Body>
+                            </Card>
+                        
+                        </Col>
+                    </Row>
+                </Container>
 
         )
     }
