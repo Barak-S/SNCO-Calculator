b0b2907e3195ea8092b937477ca46eb61beb602f
Barak Saidoff <barak.saidoff@gmail.com>
2020-08-16 11:27:21 -0400

    removed styling library for number input

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Input, Row, Col, InputNumber } from "antd";
 import { InputGroup } from 'react-bootstrap';
 
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+// import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 
 
@@ -14,8 +14,8 @@ export default class CurrencyInput extends Component {
                         name={this.props.name}
                         formatter={ value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
                         parser={ value => value.replace(/\$\s?|(,*)/g, '')}
-                        style={{ width: "100%", height: 39, fontSize: 17, paddingTop: 4 }}
-                        value={ this.props.value }
+                        style={{ width: "100%",  fontSize: 17, paddingTop: 4 }}
+                        value={ this.props.value? this.props.value : 0}
                         onChange={e => this.props.handleChange(this.props.name,e)}
                         disabled={this.props.disabled}
                     />
