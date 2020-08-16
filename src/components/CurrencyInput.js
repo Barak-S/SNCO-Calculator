8befcf74ca7588ac88a6b69e585753663b266b14
Barak Saidoff <barak.saidoff@gmail.com>
2020-08-16 15:23:11 -0400

    sunday

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Input, Row, Col, InputNumber } from "antd";
+import { Input, Row, Col, InputNumber, Form } from "antd";
 import { InputGroup } from 'react-bootstrap';
 
 // import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
@@ -9,18 +9,24 @@ import { InputGroup } from 'react-bootstrap';
 export default class CurrencyInput extends Component {
     render() {
         return (
-            <div>
-                    <InputNumber
-                        name={this.props.name}
-                        formatter={ value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                        parser={ value => value.replace(/\$\s?|(,*)/g, '')}
-                        style={{ width: "100%",  fontSize: 17, paddingTop: 4 }}
-                        value={ this.props.value? this.props.value : 0}
-                        onChange={e => this.props.handleChange(this.props.name,e)}
-                        disabled={this.props.disabled}
-                    />
-            
-            </div>
+                
+                <Row gutter={16}>
+                    <Col xs={24} md={12}>
+
+                        <InputNumber
+                            name={this.props.name}
+                            formatter={ value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={ value => value.replace(/\$\s?|(,*)/g, '')}
+                            style={{ width: "100%",  fontSize: 17, paddingTop: 4, display:"flex" }}
+                            value={ this.props.value? this.props.value : 0}
+                            onChange={e => this.props.handleChange(this.props.name,e)}
+                            disabled={this.props.disabled}
+                        />
+
+                    </Col>
+                </Row>
+                
+
         )
     }
 }
